208972d05a8f0e47658611dc91c7fad3964c7743
shargent152 <shargent152@email.com>
2026-04-23 12:52:16 -0400

    what i was able to get done with room reservation this will still load fine so we can show off the ui

--- room-reservation-sprint2/reservation-manager/src/main/java/com/example/reservation_manager/messaging/ReservationEventPublisher.java
+++ room-reservation-sprint2/reservation-manager/src/main/java/com/example/reservation_manager/messaging/ReservationEventPublisher.java
@@ -2,10 +2,15 @@ package com.example.reservation_manager.messaging;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
-public class BookingEventPublisher {
+public class ReservationEventPublisher {
     private final  RabbitTemplate rabbitTemplate;
 
-    public BookingEventPublisher(RabbitTemplate rabbitTemplate) {
+    public ReservationEventPublisher(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
+    //public void publishReservationCreated(Reservation reservation){
+      //  ReservationCreatedEvent event = new ReserverationCreatedEvent(reservation.getId(), reservation.getStatus()){
+
+        //}
+    //}
 }
