0e9240c926463a86a68ae83821406c7b0b0db9e2
ChimaPhilipsIU <chphilip@iu.edu>
2026-04-30 09:32:01 -0400

    Update RoomsController.java

--- room-reservation-sprint2/demo/src/main/java/com/example/project2/demo/controller/RoomsController.java
+++ room-reservation-sprint2/demo/src/main/java/com/example/project2/demo/controller/RoomsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
-@Controller
+@RestController
 public class RoomsController {
 
     private final RoomsService roomsService;
