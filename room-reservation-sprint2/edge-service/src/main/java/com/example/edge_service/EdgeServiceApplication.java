48745617a24a8050e2d83b15cc11722ac25d9f5d
ChimaPhilipsIU <chphilip@iu.edu>
2026-04-30 09:07:42 -0400

    Update EdgeServiceApplication.java

--- room-reservation-sprint2/edge-service/src/main/java/com/example/edge_service/EdgeServiceApplication.java
+++ room-reservation-sprint2/edge-service/src/main/java/com/example/edge_service/EdgeServiceApplication.java
@@ -2,10 +2,23 @@ package com.example.edge_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class EdgeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EdgeServiceApplication.class, args);
     }
+
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.justOrEmpty(
+            exchange.getRequest().getRemoteAddress()
+        ).map(addr -> addr.getAddress().getHostAddress())
+         .defaultIfEmpty("unknown");
+    }
 }
+
